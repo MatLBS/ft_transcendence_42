1edee52a2c625a68713894d87436d7270018c2c3
MatLBS <135004599+MatLBS@users.noreply.github.com>
2025-04-02 19:36:58 +0200

    add new tables in prisma.schema. The players of a tournament are now stored in the db

--- srcs/controllers/createTournament.js
+++ srcs/controllers/createTournament.js
@@ -1,5 +1,34 @@
+import { createTournamentDb } from '../dist/prisma/seed.js';
 
+function getRandomNumber(playerIds) {
+	const randomIndex = Math.floor(Math.random() * playerIds.length);
+	const randomNumber = playerIds[randomIndex];
+	playerIds.splice(randomIndex, 1);
+	return randomNumber;
+}
 
 export const createTournament = async (req, reply) => {
-	console.log(req.body.playerData)
+
+	let playerIds = [];
+	var playersInfos = new Map();
+
+	for (let i = 1; i < req.body.playerData.length + 1; ++i) {
+		playerIds.push(i);
+	}
+
+	for (let i = 0; i < req.body.playerData.length; ++i) {
+		playersInfos.set(i, {
+			name: req.body.playerData[i],
+			NbVictory: 0,
+			rank: 0,
+			playerNumber: getRandomNumber(playerIds),
+		});
+	}
+
+	try {
+		createTournamentDb(playersInfos)
+	} catch (error) {
+		return reply.send({message: error.message});
+	}
+
 };
