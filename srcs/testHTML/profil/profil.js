58ebc7f0d73b700af0a16ad53a37af084820536d
MatLBS <135004599+MatLBS@users.noreply.github.com>
2025-04-07 10:36:37 +0200

    Improve profil.ejs

---
+++ srcs/testHTML/profil/profil.js
@@ -0,0 +1,7 @@
+const match__history = document.getElementById('match__history');
+const graphs = document.getElementById('graphs');
+const match__results = document.getElementById('match__results');
+const match__scores = document.getElementById('match__scores');
+const match__opponents = document.getElementById('match__opponents');
+
+
