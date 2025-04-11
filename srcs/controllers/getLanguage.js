c8f277999e4444c8c5ab564b4a4aa4b21ad2c8e3
MatLBS <135004599+MatLBS@users.noreply.github.com>
2025-04-11 14:44:17 +0200

    trying to make language preferences works

--- srcs/controllers/getLanguage.js
+++ srcs/controllers/getLanguage.js
@@ -3,6 +3,7 @@ import path from 'path';
 import i18next from '../public/languages/i18n.js';
 import { authenticateUser } from "./tokens.js";
 import { findUserById, updateUserLanguage, updateSiteLanguage, getSiteLanguage } from "../dist/prisma/seed.js";
+import { createInstance } from 'i18next';
 
 export const getLanguage = async (req, reply) => {
 	let translations;
@@ -16,34 +17,43 @@ export const getLanguage = async (req, reply) => {
 	const siteLanguage = await getSiteLanguage();
 	let newLanguage = !siteLanguage ? "en" : siteLanguage.language;
 
+	console.log("ici")
+	console.log(newLanguage)
 	//Vérifier si l'utilisateur est connecté
 	if (response.status === 200) {
+		console.log("ici1")
 		const user = await findUserById(response.user.id);
-		if (language === "" && user.language !== "" && user.language !== null) { //Si language est vide c'est que l'utilisateur se connecte/reconnecte pour la première fois, s'il a une langue de préférence je la mets
+		console.log(user.language)
+		console.log(language)
+		if (language === "" && user.language !== "" && user.language !== null) { //Si l'utilisateur a une langue de préférence et qu'il vient de se reconnecter ou rafraichir la page je la mets
+			console.log("if")
 			language = user.language;
 			await updateSiteLanguage(language)
 		}
 		else if (language !== "") { //Si l'utilisateur change de langue
+			console.log("else if")
 			await updateSiteLanguage(language);
 			await updateUserLanguage(user.id, language)
 		}
 		else { //Language est vide, l'utilisateur n'a pas de langue par défaut, je mets la langue du site
+			console.log("else")
 			language = newLanguage;
 			await updateUserLanguage(user.id, language)
 		}
 		translations = i18next.getResourceBundle(language, 'translation');
 	} 
 	else {
+		console.log("ici2")
+		console.log(language)
+		console.log(newLanguage)
 		if (language === "" && newLanguage !== "") { //Si language est vide et siteLanguage non, c'est que l'utilisateur rafraichit la page
+			console.log("if")
 			await updateSiteLanguage(newLanguage);
 			language = newLanguage;
 		}
 		else if (language !== "") {//Si l'utilisateur change de langue
+			console.log("else if")
 			await updateSiteLanguage(language);
-		}
-		else { //Si language est vide et siteLanguage aussi, c'est que l'utilisateur se connecte pour la première fois sur le site, je met alors la langue par défaut, anglais(en)
-			await updateSiteLanguage("en");
-			language = "en";
 		}
 		translations = i18next.getResourceBundle(language, 'translation');
 	}
