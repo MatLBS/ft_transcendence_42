4228bf7f75075d308c2458d126d4338214f53419
msima0 <28370316+msima0@users.noreply.github.com.>
2025-05-12 14:41:07 +0200

    fix: other user profile

--- srcs/controllers/getUserProfile.js
+++ srcs/controllers/getUserProfile.js
@@ -6,21 +6,27 @@ import { errorPage } from './errorPage.js';
 
 export const getUserProfile = async (req, reply, username) => {
 	const response = await authenticateUser(req);
-	if (!response.status === 200 || !(user = await findUserById(response.user.id))) {
-		return await errorPage(req, reply, response, 401);
+	try {
+		let userLog = await findUserById(response.user.id);
+		if (!response.status === 200 || !userLog) {
+			return await errorPage(req, reply, response, 401);
+		}
+		const jsonLanguage = req.body.jsonLanguage;
+		const { page } = req.params;
+		const finalPage = page ?? username;
+		const user = await findUser(finalPage);
+		if (!user)
+			return await errorPage(req, reply, response, 404);
+
+		const isConnected = response.status === 200;
+		const himself = isConnected && response.user.id === user.id;
+		const friend = isConnected ? await isFriend(response.user.id, user.id) : false;
+
+		const content = await ejs.renderFile("/usr/src/app/srcs/views/users/viewProfile.ejs", { user, jsonLanguage, isConnected, friend, himself });
+		let css = "/public/style/users/viewProfile.css", js = "/dist/srcs/public/scripts/users/viewProfile.js";
+		return reply.send({ content, css, js, isConnected });
+	} catch (error) {
+		console.error(error);
+		return await errorPage(req, reply, response, 500);
 	}
-	const jsonLanguage = req.body.jsonLanguage;
-	const { page } = req.params;
-	const finalPage = page ?? username;
-	const user = await findUser(finalPage);
-	if (!user)
-		return await errorPage(req, reply, response, 404);
-
-	const isConnected = response.status === 200;
-	const himself = isConnected && response.user.id === user.id;
-	const friend = isConnected ? await isFriend(response.user.id, user.id) : false;
-
-	const content = await ejs.renderFile("/usr/src/app/srcs/views/users/viewProfile.ejs", { user, jsonLanguage, isConnected, friend, himself });
-	let css ="/public/style/users/viewProfile.css", js = "/dist/srcs/public/scripts/users/viewProfile.js";
-	return reply.send({ content, css, js, isConnected });
 }
